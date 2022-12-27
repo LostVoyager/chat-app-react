567acd5c906ba6f1809c71356c492eb4a77c29a4
LostVoyager <Viktorfinger@hotmail.com>
2022-12-27 02:12:14 -0300

    Create header on top of the sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">React Chat</span>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>John Doe</span>
+        <button>Logout</button>
+      </div>
+    </div>
   )
 }
