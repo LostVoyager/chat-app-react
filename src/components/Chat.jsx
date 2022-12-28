8556676a3dd947d881eea0d2b2bb4d5ec12bc6b3
LostVoyager <Viktorfinger@hotmail.com>
2022-12-28 03:19:54 -0300

    Create components to send messages - input and send button

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Cam from '../img/cam.png'
+import Add from '../img/add.png'
+import More from '../img/more.png'
+import Messages from './Messages'
+import Input from './Input'
+
 
 export default function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Jane Doe</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" srcset="" />
+          <img src={Add} alt="" srcset="" />
+          <img src={More} alt="" srcset="" />
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Chats() {
   return (
-    <div>Chats</div>
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/9588984/pexels-photo-9588984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcset="" />
+        <div className="userChatInfo">
+          <span>Jane Doe</span>
+          <p>Hello world!</p>
+        </div>
+      </div>
+    </div>
   )
 }
