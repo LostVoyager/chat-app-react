3f410d838c39a7b8a0f8acfcc2886de78a2860b0
LostVoyager <Viktorfinger@hotmail.com>
2022-12-29 02:14:47 -0300

    Create individual message component and add some scss responsiveness

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://images.pexels.com/photos/9588984/pexels-photo-9588984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello world!</p>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
