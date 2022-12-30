6db983cbf6eddae067c16305e09df50601cc776e
LostVoyager <Viktorfinger@hotmail.com>
2022-12-30 02:06:20 -0300

    Integrate with firebase and add user registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from '../img/addAvatar.png'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+
+
 
 export default function Register() {
+
+  const [err,setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try{
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, res.user.uid );
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+          console.log(err);
+          alert(err);
+        }, 
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName: displayName,
+              photoURL: downloadURL
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL
+            })
+            await setDoc(doc(db, "userChats", res.user.uid), {})
+          });
+        }
+      );
+    }
+    catch(err){
+      setErr(true);
+      console.log(err);
+      alert(err);
+    }
+  }
+
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className='logo'>Chat App</span>
             <span className='title'>Register</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" name="" id="" placeholder='display name'/>
                 <input type="email" name="" id="" placeholder='email'/>
                 <input type="password" name="" id="" placeholder='password'/>
@@ -17,6 +74,7 @@ export default function Register() {
                   <span>Add an avatar</span>
                 </label>
                 <button>Sign Up</button>
+                {err && <span>Something went wrong. Please try again later.</span>}
             </form>
             <p>Already registered? Log in</p>
         </div>
